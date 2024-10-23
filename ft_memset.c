a9b1aae4853c1ddf9bebc7e157b3701bdc71aaca
Julio Henrique Oliveira Ramos <juhenriq@c2r1p6.42sp.org.br>
2024-10-22 21:37:37 -0300

    ei ou procedimento padrao

--- ft_memset.c
+++ ft_memset.c
@@ -1,16 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juhenriq <dev@juliohenrique.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/21 20:02:22 by juhenriq          #+#    #+#             */
-/*   Updated: 2024/10/22 20:41:31 by juhenriq         ###   ########.fr       */
+/*   Created: 2024/10/22 20:45:13 by juhenriq          #+#    #+#             */
+/*   Updated: 2024/10/22 21:18:36 by juhenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isprint(int c)
+#include <stdlib.h>
+
+void	*ft_memset(void *s, int c, size_t n)
 {
-	return ((c >= 32) && (c <= 126));
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *) s)[i] = (unsigned char) c;
+		i++;
+	}
+	return (s);
 }
