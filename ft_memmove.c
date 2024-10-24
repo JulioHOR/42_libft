acdcbc6f5146768f92605af21497de6c2cb92091
Julio Henrique Oliveira Ramos <juhenriq@c2r1p6.42sp.org.br>
2024-10-24 19:00:02 -0300

    é nois de novo

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: juhenriq <dev@juliohenrique.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 17:46:29 by juhenriq          #+#    #+#             */
-/*   Updated: 2024/10/23 20:43:22 by juhenriq         ###   ########.fr       */
+/*   Updated: 2024/10/24 18:59:46 by juhenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,29 +38,3 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-
-// #include <stdio.h>
-// #include <string.h>
-// int	main(void)
-// {
-// 	char dst1[240];
-// 	char dst2[240];
-// 	char *data = "vamos simplificar essa string";
-// 	int size = 240 - 15;
-
-// 	memset(dst1, 'A', sizeof(dst1));
-// 	memset(dst2, 'A', sizeof(dst2));
-// 	memcpy(dst1, data, strlen(data));
-// 	memcpy(dst2, data, strlen(data));
-// 	memmove(dst1, dst1 + 3, size);
-// 	ft_memmove(dst2, dst2 + 3, size);
-// 	if (!memcmp(dst1, dst2, size))
-// 		printf("PASSOU!");
-// 	else
-// 	{
-// 		printf("tem algo errado\n\n");
-// 		printf("dst1 é: %s\n\n", dst1);
-// 		printf("dst2 é: %s\n\n", dst2);
-// 	}
-// 	return (0);
-// }
