d5f5c09c21a92864ba972a4aed5abfe413271e73
Julio Henrique <juliohor@JHOR-DellG15.>
2024-11-03 01:46:45 -0300

    mais um progresso

---
+++ ft_memchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhenriq <dev@juliohenrique.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 01:23:07 by juhenriq          #+#    #+#             */
+/*   Updated: 2024/11/03 01:36:04 by juhenriq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	c = (unsigned char) c;
+	i = 0;
+	while (i < n)
+	{
+		if(((unsigned char *)s)[i] == (unsigned char)c)
+			return (void *)&((unsigned char *)s)[i];
+		i++;
+	}
+	return ((void *) 0);
+}
