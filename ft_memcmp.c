cbd192757347b8a24448532ebeb9565fa0c75d77
Julio Henrique <dev@juliohenrique.com>
2024-11-07 14:35:25 -0300

    agora podemos trabalhar na 42 novamente

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,22 +6,24 @@
 /*   By: juhenriq <dev@juliohenrique.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 01:38:03 by juhenriq          #+#    #+#             */
-/*   Updated: 2024/11/06 18:49:08 by juhenriq         ###   ########.fr       */
+/*   Updated: 2024/11/07 14:31:47 by juhenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-int	memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
 	while (i < n)
 	{
 		if (((unsigned char *)(s1))[i] != ((unsigned char *)(s2))[i])
-		{
-			
-		}
+			return (((unsigned char *)(s1))[i] - ((unsigned char *)(s2))[i]);
+		i++;
 	}
+	return (0);
 }
