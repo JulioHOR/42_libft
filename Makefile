63ce2c677d5961cec537cbc6cbec35224381d469
Julio Henrique Oliveira Ramos <juhenriq@c2r1p6.42sp.org.br>
2024-11-07 16:54:27 -0300

    antes da mudança do atoi

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: juhenriq <dev@juliohenrique.com>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/17 19:42:29 by juhenriq          #+#    #+#              #
-#    Updated: 2024/11/07 14:32:59 by juhenriq         ###   ########.fr        #
+#    Updated: 2024/11/07 16:01:49 by juhenriq         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,7 +19,7 @@ CFLAGS = -Wall -Wextra -Werror
 SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
 	ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c ft_strlcpy.c \
 	ft_strlcat.c ft_toupper.c ft_tolower.c ft_strchr.c ft_strrchr.c \
-	ft_strncmp.c ft_memchr.c ft_memcmp.c ft_strnstr.c ft_atoi.c \
+	ft_strncmp.c ft_memchr.c ft_memcmp.c ft_strnstr.c 
 
 OBJS = $(SRCS:.c=.o)
 
--- ft_atoi.c
+++ ft_atoi.c
@@ -6,27 +6,48 @@
 /*   By: juhenriq <dev@juliohenrique.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 18:39:51 by juhenriq          #+#    #+#             */
-/*   Updated: 2024/11/07 14:27:37 by juhenriq         ###   ########.fr       */
+/*   Updated: 2024/11/07 16:53:32 by juhenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include <limits.h>
 // REMOVA ABAIXO
 #include <stdio.h>
 
-int	return_number()
-{
-	
-}
-
 int	get_number_size_as_str(const char *nptr)
 {
 	int	i;
 
-	i = 0;
+	i = 1;
 	while (nptr[i] != '\0')
 		i++;
-	return (i);
+	return (--i);
+}
+
+int	return_number(char *valid_chars)
+{
+	int	number;
+	int	number_size_as_str;
+	int	multiplier;
+	int	i;
+
+	number = 0;
+	multiplier = 1;
+	number_size_as_str = get_number_size_as_str(valid_chars);
+	i = number_size_as_str;
+	while (i > 0)
+	{
+		number += (valid_chars[i--] - '0') * multiplier;
+		multiplier *= 10;
+	}
+	if (valid_chars[0] == '-')
+	{
+		if (number == 2147483647)
+			return (-2147483648);
+		return (number * -1);
+	}
+	return (number);
 }
 
 // 2.147.483.647 - são 10 caracteres
@@ -44,33 +65,32 @@ int	ft_atoi(const char *nptr)
 	pos = 0;
 	while ((((nptr[i] >= '\t') && (nptr[i] <= '\r')) || (nptr[i] == ' ')) || \
 		((nptr[i] >= '+') && (nptr[i] <= '-')) || \
-		((nptr[i] >= '0') && (nptr[i] <= '9')) && nptr[i] != '\0')
+		((nptr[i] >= '0') && (nptr[i] <= '9')) && nptr[i] != '\0' && pos <= 11)
 	{
 		if (nptr[i] == '+' || nptr[i] == '-')
 		{
 			valid_chars[pos++] = nptr[i];
 			number_must_start = 1;
 		}
-		if (((nptr[i] >= '0') && (nptr[i] <= '9')) || (number_must_start))
+		if (((nptr[i] >= '0') && (nptr[i] <= '9')))
 		{
+			if (!(number_must_start))
+				valid_chars[pos++] = '+';
 			valid_chars[pos++] = nptr[i];
 			number_must_start = 1;
 		} else 
-		{
-			
-		}
+			if (!(number_must_start))
+				break;
 		i++;
 	}
+	valid_chars[pos] = '\0';
 	nb_str_size = get_number_size_as_str(nptr);
-	if (nb_str_size > 10)
-		return (0);
-	while (nptr[i] )
-	return (0);
+	return (return_number(valid_chars));
 }
 
 int	main(void)
 {
-	const char	*nptr = "2147483647";
+	const char	*nptr = "9999999999";
 	
 	ft_atoi(nptr);
 	return (0);
