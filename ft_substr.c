83ab7d5fa7cbab2470d565593fc38f533a7056ac
Julio Henrique Oliveira Ramos <juhenriq@c2r1p6.42sp.org.br>
2024-11-07 21:08:49 -0300

    passou o ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,25 +6,31 @@
 /*   By: juhenriq <dev@juliohenrique.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 18:10:14 by juhenriq          #+#    #+#             */
-/*   Updated: 2024/11/07 19:11:18 by juhenriq         ###   ########.fr       */
+/*   Updated: 2024/11/07 21:05:37 by juhenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*new_substring;
+	size_t	s_len;
+	size_t	real_substring_len;
 	size_t	i;
 
-	new_substring = (char *) malloc((len + 1) * sizeof(char));
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	real_substring_len = len;
+	if (len > (s_len - start))
+		real_substring_len = s_len - start;
+	new_substring = (char *) malloc((real_substring_len + 1));
+	if (!new_substring)
+		return (((void *) 0));
 	i = 0;
-	while ((i < len) && (s[start] != '\0'))
-	{
-		new_substring[i] = s[start];
-		start++;
-		i++;
-	}
+	while ((i < real_substring_len) && (s[start] != '\0'))
+		new_substring[i++] = s[start++];
 	new_substring[i] = '\0';
 	return (new_substring);
 }
