6ed420406424687bec6beb09d6e254bc54bc2fa7
Julio Henrique Oliveira Ramos <juhenriq@c2r1p6.42sp.org.br>
2024-11-09 01:53:34 -0300

    na metade do split 2

--- ft_split.c
+++ ft_split.c
@@ -6,42 +6,38 @@
 /*   By: juhenriq <dev@juliohenrique.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 22:04:02 by juhenriq          #+#    #+#             */
-/*   Updated: 2024/11/09 01:22:54 by juhenriq         ###   ########.fr       */
+/*   Updated: 2024/11/09 01:53:23 by juhenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <unistd.h>
 
 // continue daqui
 void	fill_pointer_array(char **pointer_array, char const *s, char c)
 {
 	int	pointer_array_pos;
 	int	start;
-	int	end;
 	int	i;
 
+	pointer_array_pos = 0;
 	start = -1;
-	end = -1;
 	i = 0;
 	while(s[i])
 	{
-		if ((start == -1) && (i != c))
+		if ((start == -1) && (s[i] != c))
 			start = i;
-		if ((s[i] == c) && (start == -1))
+		if (s[i] == c)
 		{
-			pointer_array[pointer_array_pos++] = calloc(1, 1);
-			if (s[i + 1] != '\0' && s[i] != c)
-				start = i + 1;
-		}
-		if ((s[i] == c) && (i != 0))
-		{
-			pointer_array[pointer_array_pos++] = calloc(1, 1);
-			if (s[i + 1] != '\0' && s[i] != c)
-				start = i + 1;
-		}
-		if ((s[i] == c) && (s[i + 1] == '\0'))
-		{
-			pointer_array[pointer_array_pos++] = calloc(1, 1);
+			if (start != -1)
+			{
+				pointer_array[pointer_array_pos++] = \
+					ft_substr(s, start, (i + 1));
+				start = -1;
+			}
+			if ((start == -1) && (s[i + 1] == '\0'))
+				pointer_array[pointer_array_pos++] = calloc(1, 1);
 		}
 		i++;
 	}
@@ -74,14 +70,13 @@ char	**ft_split(char const *s, char c)
 	unsigned int	pointer_array_len;
 
 	pointer_array_len = determine_pointer_array_len(s, c);
-	pointer_array = (char **) malloc(pointer_array_len);
+	pointer_array = (char **) malloc(pointer_array_len * sizeof(char *));
 	pointer_array[pointer_array_len - 1] = ((void *) 0);
 	fill_pointer_array(pointer_array, s, c);
+	printf("\napenas para debug:\n- pointer_array_len foi: %d", pointer_array_len);
 	return (pointer_array);
 }
 
-#include <stdio.h>
-#include <unistd.h>
 int	main(void)
 {
 	// quantidade de 'c's = 8
@@ -89,9 +84,12 @@ int	main(void)
 	char	delimiter = 'c';
 
 	char **pointer_array = ft_split(string, delimiter);
-	write(1, '\n', 1);
-	while(*pointer_array)
-		printf("\n%s", *pointer_array++);
-	write(1, '\n', 1);
+	write(1, "\n", 1);
+	while(pointer_array)
+	{
+		printf("\n%s", *pointer_array);
+		pointer_array++;
+	}
+	write(1, "\n", 1);
 	return (0);
 }
